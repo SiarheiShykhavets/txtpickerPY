0711a27631bb96d1d99aa168b46dbc4a6163a5b7
Siarhei.Shykhavets <Siarhei.Shykhavets@infotech.edu.pl>
2026-01-07 16:55:07 +0300

    добавил немножко дебагерской фичи которая будет выводиться в терминал

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
-from pathlib import Path
-import shutil 
+from pathlib import Path #библиотека для нормальной работы путей и файлов
+import shutil #модуль для копирования файлов
 
 base_dir = Path(__file__).parent # «Возьми путь к этому скрипту и возьми папку, в которой он лежит» (папка project)
 input_dir = base_dir / "input" # «В папке проекта есть папка input» (папка input)
@@ -13,10 +13,22 @@ if not output_dir.exists():
     output_dir.mkdir()
 
 txt_files = []
+#фор отсеивает пути к тхт файлам в список, чтобы в будущем их скопировать 
 for file in input_dir.iterdir(): #проходимся по каждому файлу в папке input_dir
     if file.is_file() and file.suffix == ".txt": #ЕСЛИ файл вообще является файлом и расширение файла (suffix, библиотека Path) является txt, то
         txt_files.append(file)
-if len(txt_files) == 0:
-    print('чет пусто')
 
-        
+#конечные условия для дебага, если что то пойдет не так
+if len(list(input_dir.iterdir())) == 0: #если длина пути инпут который мы переформатировали в список является нулем то (ну короче файлов нету ваще)
+    print("папка input пуста") #спиздил у чата гпт это условие, но в целом понимаю       
+elif len(txt_files) == 0:
+    print('в папке input нету txt файлов')
+else:
+    ready = []
+    for file in txt_files:
+        shutil.copy(file, output_dir)
+        ready.append(file)
+    if ready:
+        print("тхт файлы были успешно скопированы")
+    else:
+        print("Что то пошло не так...")        
